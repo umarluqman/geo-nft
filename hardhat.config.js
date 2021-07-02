aa3a56fbb1434244f13c90858f2d8d5b488e951f
Umar Luqman <>
2021-07-03 00:04:46 +0800

    added harmony testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+let key = require("./key.json");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,5 +19,14 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: `https://api.s0.b.hmny.io`,
+      accounts: [`0x${key.HARMONY_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://api.s0.t.hmny.io`,
+      accounts: [`0x${key.HARMONY_PRIVATE_KEY}`],
+    },
+  },
 };
-
