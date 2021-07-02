5570bd616507b9585ed474a52db082c3b7908bf1
Umar Luqman <>
2021-07-03 00:06:00 +0800

    added script for the contracts

---
+++ scripts/deploy-marketplace.js
@@ -0,0 +1,38 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const marketplace = await NFTMarket.deploy();
+  const { address } = await marketplace.deployed();
+
+  console.log("Marketplace deployed to:", address);
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(address);
+
+  const { address: nftAddress } = await nft.deployed();
+
+  console.log("NFT deployed to:", nftAddress);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
