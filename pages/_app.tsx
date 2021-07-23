ebdfdfec010179e58d3ec12a8ce164e5b64be0fa
Umar Luqman <>
2021-07-23 20:50:09 +0800

    remove graphql deps and prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,15 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "src/apollo";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const client = useApollo();
-
   return (
-    <ApolloProvider client={client}>
+    <>
       <Head>
         <title>ONE World</title>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
       <Component {...pageProps} />
-    </ApolloProvider>
+    </>
   );
 }
