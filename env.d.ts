78994adbf689efcce6235bc680924109aca8c6a6
Umar Luqman <>
2021-07-23 17:57:46 +0800

    remove auth deps

--- env.d.ts
+++ env.d.ts
@@ -2,10 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_MAPBOX_API_TOKEN: string;
     NEXT_PUBLIC_FIREBASE_API_KEY: string;
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
-    FIREBASE_CLIENT_EMAIL: string;
-    FIREBASE_PRIVATE_KEY: string;
     DATABASE_URL: string;
     NEXT_PUBLIC_NFT_ADDRESS: string;
     NEXT_PUBLIC_MARKETPLACE_ADDRESS: string;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,18 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "src/apollo";
-import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo();
 
   return (
-    <AuthProvider>
-      <ApolloProvider client={client}>
-        <Head>
-          <title>ONE World</title>
-          <link rel="icon" href="/favicon.ico"></link>
-        </Head>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <Head>
+        <title>ONE World</title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
