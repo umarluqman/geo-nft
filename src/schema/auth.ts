539ec554da0d0f9c7d0b32aa1a8e92e65a95a218
Umar Luqman <>
2021-07-23 20:50:09 +0800

    remove graphql deps and prisma

--- src/schema/auth.ts
+++
@@ -1,7 +0,0 @@
-import { AuthChecker } from "type-graphql";
-import { Context } from "./context";
-
-export const authChecker: AuthChecker<Context> = ({ context }) => {
-  const { uid } = context;
-  return !!uid;
-};
