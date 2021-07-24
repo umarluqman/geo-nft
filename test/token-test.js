09338d486fd5b3f304f75c40cae46a74fc2272c5
Umar Luqman <>
2021-07-24 17:19:17 +0800

    written tests in js

---
+++ test/token-test.js
@@ -0,0 +1,240 @@
+const { ethers } = require("hardhat");
+const chai = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+chai.use(solidity);
+
+const { expect } = chai;
+
+describe("NFT marketplace contract", function () {
+  let NFTMarketplace;
+  let marketplaceContract;
+  let marketContractAddress;
+
+  let NFTToken;
+  let tokenContract;
+  let tokenContractAddress;
+
+  let ownerAddress;
+  let alice; // account 1
+  let nonOwner;
+  let otherAddressList;
+
+  beforeEach(async function () {
+    NFTMarketplace = await ethers.getContractFactory("Marketplace");
+    marketplaceContract = await NFTMarketplace.deploy();
+    await marketplaceContract.deployed();
+    marketContractAddress = marketplaceContract.address;
+
+    NFTToken = await ethers.getContractFactory("Token");
+    tokenContract = await NFTToken.deploy(marketContractAddress);
+    await tokenContract.deployed();
+    tokenContractAddress = tokenContract.address;
+
+    const [
+      owner,
+      address1,
+      address2,
+      ...otherAddresses
+    ] = await ethers.getSigners();
+
+    ownerAddress = owner.address;
+    alice = address1;
+    nonOwner = address2;
+    otherAddressList = otherAddresses;
+  });
+
+  describe("Deployment", () => {
+    it("Should set the right owner", async function () {
+      expect(await tokenContract.owner()).to.equal(ownerAddress);
+    });
+    it("Should be deployed", async function () {
+      expect(marketplaceContract).to.exist;
+      expect(tokenContract).to.exist;
+    });
+  });
+
+  describe("NFT token", () => {
+    let tokenId;
+    beforeEach(async function () {
+      const tokenURI = {
+        name: "Great Wall of China, Huairou District, China",
+        image: "QmcEKozMSmS4sV37wkuU9Xe8ctvpNaeKDfBFPRo4Uz8kFJ",
+        attributes: {
+          latitude: "40.4319077",
+          longitude: "116.5703749",
+        },
+      };
+
+      const transaction = await tokenContract.createToken(
+        JSON.stringify(tokenURI)
+      );
+      const tx = await transaction.wait();
+      const event = tx?.events?.[0];
+      const value = event?.args?.[2];
+      tokenId = value.toNumber();
+    });
+    it("Should be minted to the owner", async function () {
+      expect(await tokenContract.ownerOf(tokenId)).to.equal(ownerAddress);
+    });
+
+    it("Should approve marketplace contract for all", async function () {
+      expect(
+        await tokenContract.isApprovedForAll(
+          ownerAddress,
+          marketContractAddress
+        )
+      ).to.equal(true);
+    });
+  });
+  describe("Marketplace", () => {
+    let tokenId;
+    let price; // price of NFT
+    let listingPrice; // getting from contract function
+    let marketplaceFee; // hardcoded
+    const emptyAddress = "0x0000000000000000000000000000000000000000";
+
+    const tokenURI = {
+      name: "Great Wall of China, Huairou District, China",
+      image: "QmcEKozMSmS4sV37wkuU9Xe8ctvpNaeKDfBFPRo4Uz8kFJ",
+      attributes: {
+        latitude: "40.4319077",
+        longitude: "116.5703749",
+      },
+    };
+
+    beforeEach(async function () {
+      let transaction = await tokenContract.createToken(
+        JSON.stringify(tokenURI)
+      );
+      const tx = await transaction.wait();
+      const event = tx?.events?.[0];
+      const value = event?.args?.[2];
+      tokenId = value.toNumber();
+
+      price = ethers.utils.parseUnits("500", "ether");
+      marketplaceFee = ethers.utils.parseUnits("25", "ether"); // 5% of 500
+
+      listingPrice = await marketplaceContract.getListingPrice(price);
+
+      transaction = await marketplaceContract.createMarketItem(
+        tokenContractAddress,
+        tokenId,
+        price,
+        { value: listingPrice }
+      );
+      await transaction.wait();
+    });
+
+    it("Should be able to get 5% of the market item price", () => {
+      expect(listingPrice.toString()).to.equal(marketplaceFee.toString());
+    });
+
+    it("Should be able to fetch the created item", async () => {
+      const data = await marketplaceContract.fetchMarketItems();
+
+      const items = await Promise.all(
+        data.map(async (i) => {
+          const tokenURI = await tokenContract.tokenURI(i.tokenId);
+
+          const parsedTokenURI = JSON.parse(tokenURI);
+
+          const item = {
+            price: i.price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: parsedTokenURI.image,
+            address: parsedTokenURI.name,
+            attributes: parsedTokenURI.attributes,
+          };
+          return item;
+        })
+      );
+
+      expect(items[0].price).to.equal(price);
+      expect(items[0].tokenId).to.equal(tokenId);
+      expect(items[0].seller).to.equal(ownerAddress);
+      expect(items[0].owner).to.equal(emptyAddress);
+      expect(items[0].image).to.equal(tokenURI.image);
+      expect(items[0].address).to.equal(tokenURI.name);
+      expect(items[0].attributes.latitude).to.equal(
+        tokenURI.attributes.latitude
+      );
+      expect(items[0].attributes.longitude).to.equal(
+        tokenURI.attributes.longitude
+      );
+    });
+
+    it("Should be able to let Alice buy the token, fetch the token and list it in the marketplace", async () => {
+      const transaction = await marketplaceContract
+        .connect(alice)
+        .createMarketSale(tokenContractAddress, tokenId, {
+          value: price,
+        });
+      await transaction.wait();
+      const data = await marketplaceContract.connect(alice).fetchMyNfts();
+
+      const items = await Promise.all(
+        data.map(async (i) => {
+          const tokenURI = await tokenContract.tokenURI(i.tokenId);
+
+          const parsedTokenURI = JSON.parse(tokenURI);
+
+          const item = {
+            price: i.price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: parsedTokenURI.image,
+            address: parsedTokenURI.name,
+            attributes: parsedTokenURI.attributes,
+          };
+          return item;
+        })
+      );
+
+      expect(items[0].price).to.equal(price);
+      expect(items[0].tokenId).to.equal(tokenId);
+
+      expect(items[0].seller).to.equal(
+        ownerAddress,
+        "Item's seller should be the owner address"
+      );
+
+      expect(items[0].owner).to.equal(
+        alice.address,
+        "Item's owner should be Alice's address"
+      );
+
+      expect(items[0].image).to.equal(tokenURI.image);
+      expect(items[0].address).to.equal(tokenURI.name);
+      expect(items[0].attributes.latitude).to.equal(
+        tokenURI.attributes.latitude
+      );
+      expect(items[0].attributes.longitude).to.equal(
+        tokenURI.attributes.longitude
+      );
+
+      // //List item back after purchase
+
+      // const price = ethers.utils.parseUnits("600", "ether");
+      // const marketplaceFee = ethers.utils.parseUnits("30", "ether"); // 5% of 500
+
+      // listingPrice = await marketplaceContract.getListingPrice(price);
+
+      // transaction = await marketplaceContract.createMarketItem(
+      //   tokenContractAddress,
+      //   tokenId,
+      //   price,
+      //   { value: listingPrice }
+      // );
+      // await transaction.wait();
+
+      // expect(listingPrice.toString()).to.equal(
+      //   marketplaceFee.toString(),
+      //   "Should be 5% of the market item price"
+      // );
+    });
+  });
+});
---
+++ test/token-test.ts
@@ -0,0 +1,49 @@
+import { ethers } from "hardhat";
+import chai, { expect } from "chai";
+import { solidity } from "ethereum-waffle";
+import {
+  Token,
+  Token__factory,
+  Marketplace,
+  Marketplace__factory,
+} from "../types";
+
+chai.use(solidity);
+
+describe("Token contract", function () {
+  let NFTMarketplace: Marketplace__factory;
+  let marketplace: Marketplace;
+  let marketContractAddress;
+
+  let NFTToken: Token__factory;
+  let token: Token;
+  let tokenContractAddress;
+
+  let ownerAddress: any;
+  let buyer;
+  let address2;
+  let otherAddressList;
+
+  beforeEach(async function () {
+    NFTMarketplace = await ethers.getContractFactory("Marketplace");
+    marketplace = await NFTMarketplace.deploy();
+    await marketplace.deployed();
+    marketContractAddress = marketplace.address;
+
+    NFTToken = await ethers.getContractFactory("Token");
+    token = await NFTToken.deploy(marketContractAddress);
+    await token.deployed();
+    tokenContractAddress = token.address;
+
+    const [owner, address1, ...otherAddresses] = await ethers.getSigners();
+
+    ownerAddress = owner;
+    token.isApprovedForAll;
+    buyer = address1;
+    otherAddressList = otherAddresses;
+  });
+
+  it("Should set the right owner", async function () {
+    expect(await token.owner()).to.equal(ownerAddress);
+  });
+});
